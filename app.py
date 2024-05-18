d1c47cc3f81ad74f6561da1ae5a38e7594a21b8c
Jan de Boer <jan.de-boer@uni-konstanz.de>
2024-05-18 09:42:41 +0200

    auto set port

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, request, render_template, redirect, url_for
 
+import os
+
+port = int(os.environ.get('PORT', 5001))
+
 app = Flask(__name__)
+
+
 
 @app.route('/')
 def index():
@@ -12,5 +18,8 @@ def submit():
     # Process the prompt (this will be more complex later)
     return redirect(url_for('index'))
 
+def main():
+    app.run(host='0.0.0.0', port=port)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    main()
