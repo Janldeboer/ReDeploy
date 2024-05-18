54ab92f5b52e7aa07bf97b6a3bc22eae07cba1d9
Jan de Boer <jan.de-boer@uni-konstanz.de>
2024-05-18 09:57:18 +0200

    First AI commit
    Not self-contained yet, the change was done using ChatGPT 4o

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, request, render_template, redirect, url_for
-
+from flask import Flask, request, render_template, redirect, url_for, send_from_directory
 import os
+import subprocess
 
 port = int(os.environ.get('PORT', 5001))
 
 app = Flask(__name__)
 
+REPO_URL = 'https://github.com/Janldeboer/ReDeploy'
+CLONE_DIR = 'cloned_repo'
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -18,8 +21,32 @@ def submit():
     # Process the prompt (this will be more complex later)
     return redirect(url_for('index'))
 
+
+@app.route('/fetch_repo')
+def fetch_repo():
+    if os.path.exists(CLONE_DIR):
+        subprocess.run(['rm', '-rf', CLONE_DIR])
+    subprocess.run(['git', 'clone', REPO_URL, CLONE_DIR])
+    return redirect(url_for('view_repo'))
+
+
+@app.route('/view_repo')
+def view_repo():
+    if not os.path.exists(CLONE_DIR):
+        return "Repository not cloned yet. Please fetch the repository first."
+    
+    files = os.listdir(CLONE_DIR)
+    return render_template('view_repo.html', files=files)
+
+
+@app.route('/cloned_repo/<path:filename>')
+def serve_file(filename):
+    return send_from_directory(CLONE_DIR, filename)
+
+
 def main():
     app.run(host='0.0.0.0', port=port)
+
 
 if __name__ == '__main__':
     main()
